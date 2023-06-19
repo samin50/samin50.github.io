24aac60354cf27cb3a1cf782de565fce81c750e1
samin50 <shaheen.amin.uk@gmail.com>
2023-06-19 16:57:01 +0100

    Fix carousel bug

--- src/components/Reusables/CarouselCustom.tsx
+++ src/components/Reusables/CarouselCustom.tsx
@@ -62,10 +62,10 @@ export const CarouselCustom: React.FC<CarouselProps> = ({ imgList, slide, classN
   return (
     <div className={`${className} ${pause === true ? 'bg-white bg-opacity-[0.1] rounded-lg' : ''}  flex flex-col items-center space-y-4`}>
       <div className="relative h-96 w-full border-2 rounded-lg border-secondary dark:border-secondarydark flex items-center p-1">
-        <button onClick={prevMedia} className="absolute left-2 z-20 border-none fill-white hover:fill-gray-400 transition-colors duration-150 focus:outline-none ">
+        <button onClick={prevMedia} className="absolute left-2 z-10 border-none fill-white hover:fill-gray-400 transition-colors duration-150 focus:outline-none ">
           <svg className='h-auto w-10' fill="fill-inherit" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)matrix(-1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.05 10.157l2.122-2.121L6.05 5.914 7.464 4.5 11 8.036 7.464 11.57 6.05 10.157zM8 0c4.418 0 8 3.59 8 8 0 4.418-3.59 8-8 8-4.418 0-8-3.59-8-8 0-4.418 3.59-8 8-8zm0 2C4.693 2 2 4.686 2 8c0 3.307 2.686 6 6 6 3.307 0 6-2.686 6-6 0-3.307-2.686-6-6-6z" fillRule="evenodd"></path> </g></svg>
         </button>
-        <button onClick={nextMedia} className="absolute right-2 z-20 border-none fill-white  hover:fill-gray-400 transition-colors duration-150 focus:outline-none">
+        <button onClick={nextMedia} className="absolute right-2 z-10 border-none fill-white  hover:fill-gray-400 transition-colors duration-150 focus:outline-none">
           <svg className="h-auto w-10" fill="fill-inherit" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.05 10.157l2.122-2.121L6.05 5.914 7.464 4.5 11 8.036 7.464 11.57 6.05 10.157zM8 0c4.418 0 8 3.59 8 8 0 4.418-3.59 8-8 8-4.418 0-8-3.59-8-8 0-4.418 3.59-8 8-8zm0 2C4.693 2 2 4.686 2 8c0 3.307 2.686 6 6 6 3.307 0 6-2.686 6-6 0-3.307-2.686-6-6-6z" fillRule="evenodd"></path> </g></svg>
         </button>
         
